52f94cca3e9893eafdc07005772cc235e0d962f2
Hess Wang <hess90wang@gmail.com>
2024-04-04 20:51:17 +0900

    for streamlit repo

--- app.py
+++ app.py
@@ -26,37 +26,12 @@ st.markdown("""
             
             Use this chatbot to ask questions to an AI about your files!
             
-            ⬅️ Upload your files on the sidebar.
+            ⬅️ Write your OPENAI API KEY on the sidebar.
             """)
+st.sidebar.link_button("🏠 Git Repo 🏠", 'https://github.com/hesshess/python-gpt')
 
-with st.sidebar:
-    file = st.file_uploader(
-    "Upload a .txt .pdf or .dcx file",
-    type=["pdf", "txt", "docs"]
-)
-
-class ChatCallBackHandler(BaseCallbackHandler):
-    message = ''
-    message_b = st.empty()
-    def on_llm_start(self, *arg, **kwargs):
-        self.message_b = st.empty()
-    def on_llm_end(self, *arg, **kwargs):
-         save_message(self.message, 'ai')
-    def on_llm_new_token(self, token: str, *arg, **kwargs):
-        self.message +=token
-        self.message_b.markdown(self.message)
-
-llm = ChatOpenAI(
-    temperature=0.1,
-    streaming=True,
-    callbacks=[
-        ChatCallBackHandler(),
-    ]
-)
-memory = ConversationBufferMemory(
-    memory_key='chat_history',
-    return_messages=True,
-)
+def save_message(message, role):
+    st.session_state['messages'].append({'role': role, 'message': message})
 
 @st.cache_data(show_spinner="Embedding file...")
 def embed_file(file):
@@ -80,8 +55,7 @@ def embed_file(file):
     retriever = vectorstore.as_retriever()
     return retriever
 
-def save_message(message, role):
-    st.session_state['messages'].append({'role': role, 'message': message})
+
 
 def send_message(message, role, save=True):
     with st.chat_message(role):
@@ -95,41 +69,74 @@ def paint_history():
         
 def format_docs(docs):
     return "\n\n".join(document.page_content for document in docs)
- 
+
 def load_memory(_):
     return memory.load_memory_variables({})["chat_history"]
 
 def invoke_chain(question):
-    result = chain.invoke({
-        'question': question
-    }).content
-    memory.save_context({'input': question}, {'output': str(result)})
+    result = chain.invoke(question)
+    memory.save_context({'input': question}, {'output': str(result.content)})
 
-prompt = ChatPromptTemplate.from_messages([
+
+class ChatCallBackHandler(BaseCallbackHandler):
+    message = ''
+    message_b = st.empty()
+    def on_llm_start(self, *arg, **kwargs):
+        self.message_b = st.empty()
+    def on_llm_end(self, *arg, **kwargs):
+         save_message(self.message, 'ai')
+    def on_llm_new_token(self, token: str, *arg, **kwargs):
+        self.message +=token
+        self.message_b.markdown(self.message)
+
+
+
+api_key = st.sidebar.text_input("Please enter your OPENAI API KEY🔑")
+        
+if api_key:
+
+    llm = ChatOpenAI(
+        temperature=0.1,
+        streaming=True,
+        callbacks=[
+            ChatCallBackHandler(),
+        ],
+        openai_api_key=api_key,
+    )
+    memory = ConversationBufferMemory(
+        memory_key='chat_history',
+        return_messages=True,
+    )
+    
+    prompt = ChatPromptTemplate.from_messages([
     ('system', """
-     Answer the question using ONLY the following context. If you don't know the answer, just say you don't know. Don't make anything up!
-     
-     Context: {context}
-     """),
+    Answer the question using ONLY the following context. If you don't know the answer, just say you don't know. Don't make anything up!
+    
+    Context: {context}
+    """),
     MessagesPlaceholder(variable_name='chat_history'),
     ('human', '{question}')
 ])
 
-
-if file:
-    retriever = embed_file(file)
-    send_message("I'm ready! Ask away!", "ai", save=False)
-    paint_history()
-    message = st.chat_input("Ask anything about your file...")   
-    if message:
-        send_message(message, "human")
-        chain = ({
-            'context': retriever | RunnableLambda(format_docs),
-            'question': RunnablePassthrough(),
-            'chat_history': RunnableLambda(load_memory)
-        } | prompt | llm)
-        with st.chat_message('ai'):
-            response = chain.invoke(message) 
-        
-else:
-    st.session_state['messages'] = [] 
+    with st.sidebar:
+        file = st.file_uploader(
+        "Upload a .txt .pdf or .dcx file",
+        type=["pdf", "txt", "docs"])
+    
+    if file:
+        retriever = embed_file(file)
+        send_message("I'm ready! Ask away!", "ai", save=False)
+        paint_history()
+        message = st.chat_input("Ask anything about your file...")   
+        if message:
+            send_message(message, "human")
+            chain = ({
+                'context': retriever | RunnableLambda(format_docs),
+                'question': RunnablePassthrough(),
+                'chat_history': RunnableLambda(load_memory)
+            } | prompt | llm)
+            with st.chat_message('ai'):
+                response = invoke_chain(message) 
+            
+    else:
+        st.session_state['messages'] = [] 
