d07357d6a365772fd4fa80b7e296904eebf3d3dd
Hess Wang <hess90wang@gmail.com>
2024-04-05 01:55:19 +0900

    asfd

--- app.py
+++ app.py
@@ -34,10 +34,10 @@ def save_message(message, role):
 def embed_file(file):
     file_content = file.read()
     file_path = f"./files/{file.name}"
-    os.makedirs(file_path)
+    os.makedirs(file_path, exist_ok=True)
     with open(file_path, 'wb') as f:
         f.write(file_content)
-    os.makedirs(f"./embeddings/{file.name}")
+    os.makedirs(f"./embeddings/{file.name}", exist_ok=True)
     cache_dir = LocalFileStore(f"./embeddings/{file.name}")
     splitter = CharacterTextSplitter.from_tiktoken_encoder(
         separator="\n",
