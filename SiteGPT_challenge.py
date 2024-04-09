bb034c6f6660b4424640a2e28b6cc80e4b131b82
Hess Wang <hess90wang@gmail.com>
2024-04-09 21:53:18 +0900

    siteGPT code challenge complete

--- SiteGPT_challenge.py
+++ SiteGPT_challenge.py
@@ -1,10 +1,3 @@
-# 1st data loader : using playwright chromium
-
-# from langchain.document_loaders import AsyncChromiumLoader
-# from langchain.document_transformers import Html2TextTransformer
-
-# 2nd data loader : sitemap.xml SitemapLoader
-from tokenize import OP
 from langchain.document_loaders import SitemapLoader
 from langchain.schema.runnable import RunnableLambda, RunnablePassthrough
 from langchain.text_splitter import RecursiveCharacterTextSplitter
@@ -14,36 +7,33 @@ from langchain.chat_models import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
 import streamlit as st
 
-st.set_page_config(
-    page_title="SiteGPT",
-    page_icon="🖥️",
-)
-# html2text_transformer = Html2TextTransformer()
-
-llm = ChatOpenAI(temperature=0.1)
 
 answers_prompt = ChatPromptTemplate.from_template(
     """
     Using ONLY the following context answer the user's question. If you can't just say you don't know, don't make anything up.
-                                                
+                                                  
     Then, give a score to the answer between 0 and 5.
+
     If the answer answers the user question the score should be high, else it should be low.
+
     Make sure to always include the answer's score even if it's 0.
+
     Context: {context}
-                                                
+                                                  
     Examples:
-                                                
+                                                  
     Question: How far away is the moon?
     Answer: The moon is 384,400 km away.
     Score: 5
-                                                
+                                                  
     Question: How far away is the sun?
     Answer: I don't know
     Score: 0
-                                                
+                                                  
     Your turn!
+
     Question: {question}
-    """
+"""
 )
 
 
@@ -53,10 +43,9 @@ def get_answers(inputs):
     answers_chain = answers_prompt | llm
     # answers = []
     # for doc in docs:
-    #     result = answers_chain.invoke({
-    #         'question': question,
-    #         'context': doc.page_content
-    #     })
+    #     result = answers_chain.invoke(
+    #         {"question": question, "context": doc.page_content}
+    #     )
     #     answers.append(result.content)
     return {
         "question": question,
@@ -73,29 +62,17 @@ def get_answers(inputs):
     }
 
 
-def parse_page(soup):
-    header = soup.find("header")
-    footer = soup.find("footer")
-    if header:
-        header.decompose()
-    if footer:
-        footer.decompose()
-    return (
-        str(soup.get_text())
-        .replace("\n", " ")
-        .replace("\xa0", " ")
-        .replace("CloseSearch Submit Blog", "")
-    )
-
-
 choose_prompt = ChatPromptTemplate.from_messages(
     [
         (
             "system",
             """
             Use ONLY the following pre-existing answers to answer the user's question.
+
             Use the answers that have the highest score (more helpful) and favor the most recent ones.
+
             Cite sources and return the sources of the answers as they are, do not change them.
+
             Answers: {answers}
             """,
         ),
@@ -108,9 +85,6 @@ def choose_answer(inputs):
     answers = inputs["answers"]
     question = inputs["question"]
     choose_chain = choose_prompt | llm
-    # condensed=""
-    # for answer in answers:
-    #     condensed += f"Answer:{answer['answer']} \n Source:{answer['source'] \n Date:{answer['date']}}"
     condensed = "\n\n".join(
         f"{answer['answer']} \n Source:{answer['source']} \n Date:{answer['date']} \n "
         for answer in answers
@@ -123,9 +97,17 @@ def choose_answer(inputs):
     )
 
 
-# SitemapLoader parameter regex
-# r"^(.*\/blog\/).*",
-# r"^(?!.*\/blog\/).*", #do not contain
+def parse_page(soup):
+    sidenav = soup.find("div", class_="DocsSidebar")
+    searchnav = soup.find("div", class_="DocsToolbar")
+    footer = soup.find("footer")
+    if sidenav:
+        sidenav.decompose()
+    if footer:
+        footer.decompose()
+    if searchnav:
+        searchnav.decompose()
+    return str(soup.get_text()).replace("\n", " ")
 
 
 @st.cache_data(show_spinner="Loading website...")
@@ -134,54 +116,56 @@ def load_website(url):
         chunk_size=1000,
         chunk_overlap=200,
     )
-    # automatically clean HTML tags
     loader = SitemapLoader(
         url,
-        filter_urls=[
-            r"^(.*\/blog\/).*",
-        ],
         parsing_function=parse_page,
+        filter_urls=[r"^(.*\/(ai-gateway|vectorize|workers-ai)\/).*"],
     )
-    loader.requests_per_second = 1  # not to got blocked so making it slower to scrape
+    loader.requests_per_second = 2
     docs = loader.load_and_split(text_splitter=splitter)
     vector_store = FAISS.from_documents(docs, OpenAIEmbeddings())
     return vector_store.as_retriever()
 
 
+st.set_page_config(
+    page_title="SiteGPT",
+    page_icon="🖥️",
+)
+
+
 st.markdown(
     """
-    # SiteGPT
-            
-    Ask questions about the content of a website.
-            
-    Start by writing the URL of the website on the sidebar.
+    # SiteGPT Challenge
+    ## Ask me about CloudFlare with these subjects below
+    - AI Gateway
+    - Cloudflare Vectorize
+    - Workers AI
+
 """
 )
 
-with st.sidebar:
-    url = st.text_input(
-        "Write down a URL",
-        placeholder="https://example.com",
+
+@st.cache_data(show_spinner="guessing...")
+def invoke_query(query):
+    chain = (
+        {
+            "docs": retriever,
+            "question": RunnablePassthrough(),
+        }
+        | RunnableLambda(get_answers)
+        | RunnableLambda(choose_answer)
     )
+    result = chain.invoke(query).content
+    return result
 
 
-if url:
-    #     loader = AsyncChromiumLoader([url])
-    #     docs = loader.load()
-    #     transformed = html2text_transformer.transform_documents(docs)
-    #     st.write(docs)
-    if ".xml" not in url:
-        with st.sidebar:
-            st.error("Please write down a Sitemap URL(.xml)")
-    else:
-        retriever = load_website(url)
-        query = st.text_input("Ask a question to the website.")
-        if query:
-            chain = (
-                {"docs": retriever, "question": RunnablePassthrough()}
-                | RunnableLambda(get_answers)
-                | RunnableLambda(choose_answer)
-            )
+key = st.sidebar.text_input("⬇️ OPENAI API KEY 🔑")
 
-            result = chain.invoke(query)
-            st.markdown(result.content.replace("$", "\$"))
+if key:
+    st.session_state["key"] = key
+    llm = ChatOpenAI(temperature=0.1, openai_api_key=st.session_state["key"])
+    retriever = load_website("https://developers.cloudflare.com/assets/sitemap.xml")
+    query = st.text_input("Ask a question to the website.")
+    if query:
+        result = invoke_query(query)
+        st.markdown(str(result).replace("$", "\$"))
