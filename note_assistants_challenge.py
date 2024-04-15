fe8ae7fb0873e7938bf10afc3b0208ce2938b134
Hess Wang <hess90wang@gmail.com>
2024-04-15 18:40:49 +0900

    assistants api chall---asfdasdfa

--- note_assistants_challenge.py
+++ note_assistants_challenge.py
@@ -258,7 +258,7 @@ key = st.sidebar.text_input("⬇️ OPENAI API KEY 🔑")
 if key:
     st.session_state["key"] = key
     client = openai.Client(api_key=st.session_state["key"])
-    assistant_id = create_assistant("Research-6")
+    assistant_id = create_assistant("Research-7")
     if assistant_id:
         send_conversation("I'm ready! Let's research!", "ai", save=False)
         paint_history()
